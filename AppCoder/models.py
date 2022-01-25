ff8097d491dc67073ad1b38a5c41e431bf57ac6f
Miguel Iribarren <miribarren@MacBook-Air-de-Miguel.local>
2022-01-25 00:06:28 -0300

    Models

--- AppCoder/models.py
+++ AppCoder/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.db.models import Model
+from django.forms import CharField, EmailField, IntegerField
 
 # Create your models here.
+
+class Curso(Model):
+    asignatura = CharField(max_length=30)
+    seccion = IntegerField()
+
+class Estudiante(Model):
+    nombre = CharField(max_length=30)
+    apellido = CharField(max_length=30)
+    email = EmailField()
+
+class Profesor(Model):
+    nombre = CharField(max_length=30)
+    apellido = CharField(max_length=30)
+    email = EmailField()
+    profesion = CharField(max_length=30)
--- Proyecto/urls.py
+++ Proyecto/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from Proyecto.views import cursos, inicio, inicio_template
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('Inicio/', inicio),
+    path('Cursos', cursos),
+    path('template/', inicio_template)
 ]
